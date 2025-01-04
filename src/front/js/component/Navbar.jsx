fd95575fe3a50db5392a99ec9caefdb75299213f
Andres Villani <73246809+AndresVillani00@users.noreply.github.com>
2025-01-04 13:11:53 +0000

    [SP91] Contact list de starwars con navbar actualizados

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import starWars from "../../img/LogoStarWars.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+		<nav className="navbar navbar-expand-lg bg-dark">
 		<div className="container-fluid">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-		  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-			  <li className="nav-item">
-			  	<Link to="/list">
-					<span className="navbar-brand mb-0 h1">To do List</span>
+			<div className="col col-md-4">
+				<img src={starWars} className="img-fluid rounded-start w-25" alt="starWars" />
+			</div>
+			<div className="d-flex">
+				<Link to="/" className="text-decoration-none">
+					<span className="navbar-brand mb-0 h1 text-secondary">Home</span>
 				</Link>
-			  </li>
-			  <li className="nav-item">
-			  	<Link to="/contact">
-					<span className="navbar-brand mb-0 h1">Contact List</span>
-				</Link>
-			  </li>
-			</ul>
-		  </div>
+				<div className="collapse navbar-collapse text-end" id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+					<li className="nav-item">
+						<Link to="/list" className="text-decoration-none">
+							<span className="navbar-brand mb-0 h1 text-secondary">To do List</span>
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/contact" className="text-decoration-none">
+							<span className="navbar-brand mb-0 h1 text-secondary">Contact List</span>
+						</Link>
+					</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	  </nav>
 	);
--- src/front/js/pages/ContactList.jsx
+++ src/front/js/pages/ContactList.jsx
@@ -3,12 +3,88 @@ import trooper from "../../img/StormTrooper.png";
 
 export const ContactList = () => {
     const [ list, setList ] = useState(true);
+    const [ edit, setEdit ] = useState(false);
     const [ contactos, setContactos ] = useState([]);
+    const [ contactoParaEditar, setContactoParaEditar ] = useState([]);
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ phone, setPhone ] = useState('');
+    const [ address, setAddress ] = useState('');
     const host = 'https://playground.4geeks.com/contact';
     const slug = 'AndresVillani00';
 
+    const handleSubmitAdd = async (event) => {
+        event.preventDefault();
+        const contactoCreado = {
+            name: name,
+            phone: phone,
+            email: email,
+            address: address
+          };
+        const uri = `${host}/agendas/${slug}/contacts`;
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(contactoCreado)
+        };
+    
+        const response = await fetch(uri, options);
+        if(!response.ok){
+            console.log('Error: ', response.status, response.statusText);
+            return
+        }
+
+        getContactos();
+        setList(!list);
+    }
+
+    const handleAdd = () => {
+        setList(!list);
+        setEdit(false);
+        setContactoParaEditar('');
+        setName('');
+        setPhone('');
+        setEmail('');
+        setAddress('');
+    }
+
+    const handleSubmitEdit = async (event) => {
+        event.preventDefault();
+        const contactoEditado = {
+            name: name,
+            phone: phone,
+            email: email,
+            address: address
+          };
+        const uri = `${host}/agendas/${slug}/contacts/${contactoParaEditar.id}`;
+        const options = {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(contactoEditado)
+        };
+    
+        const response = await fetch(uri, options);
+        if(!response.ok){
+            console.log('Error: ', response.status, response.statusText);
+            return
+        }
+
+        getContactos();
+        setList(!list);
+    }
+
     const handleEdit = (taskEdit) => {
-        setList(list);
+        setList(!list);
+        setEdit(!edit);
+        setContactoParaEditar(taskEdit);
+        setName(taskEdit.name);
+        setPhone(taskEdit.phone);
+        setEmail(taskEdit.email);
+        setAddress(taskEdit.address);
     }
 
     const handleDelete = async (taskDelete) => {
@@ -23,7 +99,7 @@ export const ContactList = () => {
             return
         }
 
-        getTareas();
+        getContactos();
     }
 
     const getContactos = async () => {
@@ -47,13 +123,16 @@ export const ContactList = () => {
     }, [])
 
     return (
-        <div className="container p-5 bg-dark">
+        <div className="container p-5" style={{background: '#404040'}}>
             {
                 list === true ?
-                <div className="container my-5 p-5 bg-secondary">
-                    <h1 className="text-start">Contacts</h1>
+                <div className="container p-5 bg-dark">
+                    <div className="d-flex justify-content-between p-5">
+                        <h1 className="text-white text-start">Contacts</h1>
+                        <button type="button" onClick={() => handleAdd()} className="btn btn-secondary">Add Contact</button>
+                    </div>
                     {contactos.map((item) => 
-                        <div className="card mb-3" style={{width: '1000px'}}>
+                        <div className="card mb-3 m-auto" style={{width: '1000px'}}>
                             <div className="row g-0">
                                 <div className="col-md-4 d-flex justify-content-center ">
                                 <img src={trooper} className="img-fluid rounded-start w-50" alt="trooper" />
@@ -82,21 +161,68 @@ export const ContactList = () => {
                                     </div>
                                 </div>
                                 <div className="col-md-2 d-flex justify-content-center my-5">
-                                    <span onClick={() => handleEdit(item)} className="text-primary me-2">
+                                    <button type="submit" onClick={() => handleEdit(item)} className="btn btn-secondary me-2">
                                         <i className="fa-solid fa-pen-to-square"></i>
-                                    </span>
-                                    <span onClick={() => handleDelete(item)} className="text-danger">
+                                    </button>
+                                    <button type="submit" onClick={() => handleDelete(item)} className="btn btn-danger">
                                         <i className="fa-solid fa-trash-can"></i>
-                                    </span>
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     )}
                 </div>
                 :
-                <div>
-                    <form>
-
+                edit === true ?
+                <div className="container p-5 bg-dark">
+                    <h1 className="text-white text-start">Edit Contact</h1>
+                    <form onSubmit={handleSubmitEdit}>
+                        <div className="mb-3">
+                            <label htmlFor="inputName1" className="form-label text-secondary d-flex">Full Name<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setName(event.target.value)} value={name} type="text" className="form-control" id="inputName" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="inputEmail" className="form-label text-secondary d-flex">Email Address<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setEmail(event.target.value)} value={email} type="email" className="form-control" id="inputEmail" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="inputPhone" className="form-label text-secondary d-flex">Phone<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setPhone(event.target.value)} value={phone} type="text" className="form-control" id="inputPhone" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="inputAddress" className="form-label text-secondary d-flex">Address<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setAddress(event.target.value)} value={address} type="text" className="form-control" id="inputAddress" />
+                        </div>
+                        <div className="mb-3 text-end">
+                            <button type="submit" className="btn btn-warning me-2">Submit</button>
+                            <button type="reset" onClick={() => setList(true)} className="btn btn-secondary">Cancel</button>
+                        </div>
+                    </form>
+                </div>
+                :
+                <div className="container p-5 bg-dark">
+                    <h1 className="text-white text-start">Add Contact</h1>
+                    <form onSubmit={handleSubmitAdd}>
+                        <div className="mb-3">
+                            <label htmlFor="inputName1" className="form-label text-secondary d-flex">Full Name<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setName(event.target.value)} value={name} type="text" className="form-control" id="inputName" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="inputEmail" className="form-label text-secondary d-flex">Email Address<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setEmail(event.target.value)} value={email} type="email" className="form-control" id="inputEmail" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="inputPhone" className="form-label text-secondary d-flex">Phone<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setPhone(event.target.value)} value={phone} type="text" className="form-control" id="inputPhone" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="inputAddress" className="form-label text-secondary d-flex">Address<p className="text-warning mx-2">*</p></label>
+                            <input onChange={(event) => setAddress(event.target.value)} value={address} type="text" className="form-control" id="inputAddress" />
+                        </div>
+                        <div className="mb-3 text-end">
+                            <button type="submit" className="btn btn-warning me-2">Submit</button>
+                            <button type="reset" onClick={() => setList(true)} className="btn btn-secondary">Cancel</button>
+                        </div>
                     </form>
                 </div>
             }
