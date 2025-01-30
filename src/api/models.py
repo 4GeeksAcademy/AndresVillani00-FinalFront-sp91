326dfd382b9026e6dd90d92c72870d813e21acc0
Andres Villani <73246809+AndresVillani00@users.noreply.github.com>
2025-01-30 18:12:45 +0000

    [SP91] Modelo de datos de Instagram y de Star Wars

--- src/api/models.py
+++ src/api/models.py
@@ -5,10 +5,13 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.Boolean(), unique=False, nullable=False)
+    last_name = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -18,3 +21,86 @@ class Users(db.Model):
         return {"id": self.id,
                 "email": self.email}
     
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), unique=False, nullable=False)
+    description = db.Column(db.String(), unique=False, nullable=False)
+    body = db.Column(db.String(), unique=False, nullable=True)
+    date = db.Column(db.DateTime, unique=False, nullable=False)
+    image_url = db.Column(db.String(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=(user_id), backref=db.backref('posts_to'), lazy='select')
+
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=(user_id), backref=db.backref('user_to'), lazy='select')
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    post_to = db.relationship('Posts', foreign_keys=(post_id), backref=db.backref('media_to'), lazy='select')
+
+
+class Medias(db.Model):
+    __tablename__ = 'medias'
+    id = db.Column(db.Integer, primary_key=True)
+    media_Type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'), unique=False, nullable=False)
+    url = db.Column(db.String(), unique=True, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    post_to = db.relationship('Posts', foreign_keys=(post_id), backref=db.backref('post_to'), lazy='select')
+
+
+class Followers(db.Model):
+    __tablename__ = 'followers'
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    follower_to = db.relationship('Users', foreign_keys=(follower_id), backref=db.backref('followers_to'), lazy='select')
+    following_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    following_to = db.relationship('Users', foreign_keys=(following_id), backref=db.backref('followings_to'), lazy='select')
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    diameter = db.Column(db.String(), unique=False, nullable=False)
+    rotation_period = db.Column(db.String(), unique=False, nullable=False)
+    orbital_period = db.Column(db.String, unique=False, nullable=False)
+    gravity = db.Column(db.String(), unique=False, nullable=False)
+    population = db.Column(db.String(), unique=False, nullable=False)
+    climate = db.Column(db.String(), unique=False, nullable=False)
+    terrain = db.Column(db.String(), unique=False, nullable=False)
+
+
+class PlanetFavorites(db.Model):
+    __tablename__ = 'planet_favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=(user_id), backref=db.backref('planet_favorite_to'), lazy='select')
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    planet_to = db.relationship('Planets', foreign_keys=(planet_id), backref=db.backref('planet_to'), lazy='select')
+
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    height = db.Column(db.String(), unique=False, nullable=False)
+    mass = db.Column(db.String(), unique=False, nullable=False)
+    hair_color = db.Column(db.String, unique=False, nullable=False)
+    skin_color = db.Column(db.String(), unique=False, nullable=False)
+    eye_color = db.Column(db.String(), unique=False, nullable=False)
+    birth_year = db.Column(db.String(), unique=False, nullable=False)
+    gender = db.Column(db.String(), unique=False, nullable=False)
+
+
+class CharacterFavorites(db.Model):
+    __tablename__ = 'character_favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=(user_id), backref=db.backref('character_favorite_to'), lazy='select')
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    character_to = db.relationship('Characters', foreign_keys=(character_id), backref=db.backref('character_to'), lazy='select')
